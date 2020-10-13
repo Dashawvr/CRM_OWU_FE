e750d36b0984d8b010904654243aaec7b645c4dc
YuraNeyton <smitsh97@gmail.com>
2020-10-13 13:39:23 +0300

    add login page

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  error: string | undefined;
+  loginForm!: FormGroup;
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    // private authenticationService: AuthenticationService
+  ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    // this.isLoading = true;
+    // const login$ = this.authenticationService.login(this.loginForm.value);
+    // login$
+    //   .pipe(
+    //     finalize(() => {
+    //       this.loginForm.markAsPristine();
+    //       this.isLoading = false;
+    //     }),
+    //     untilDestroyed(this)
+    //   )
+    //   .subscribe(
+    //     (credentials) => {
+    //       log.debug(`${credentials.username} successfully logged in`);
+    //       this.router.navigate([this.route.snapshot.queryParams.redirect || '/'], { replaceUrl: true });
+    //     },
+    //     (error) => {
+    //       log.debug(`Login error: ${error}`);
+    //       this.error = error;
+    //     }
+    //   );
+  }
+
+  private createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      remember: true,
+    });
+  }
 }
