ae0d8faa21d53a5c180b7c52afd73192e17530f1
YuraNeyton <smitsh97@gmail.com>
2020-10-22 16:06:33 +0300

    added html template for applications

--- src/app/modules/applications/applications.module.ts
+++ src/app/modules/applications/applications.module.ts
@@ -1,6 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
 import {ApplicationsComponent} from './applications.component';
 import {ApplicationsRoutingModule} from './applications-routing.module';
 
@@ -9,7 +18,14 @@ import {ApplicationsRoutingModule} from './applications-routing.module';
   declarations: [ApplicationsComponent],
   imports: [
     CommonModule,
-    ApplicationsRoutingModule
+    ApplicationsRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatIconModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ]
 })
 export class ApplicationsModule {
